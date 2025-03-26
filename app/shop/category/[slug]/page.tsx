93b443e9a28fa83b58995035ac1ead7af61b8802
DMZHub <mostafaalfadil63@gmail.com>
2025-03-26 07:12:22 +0200

    Update page.tsx

--- app/shop/category/[slug]/page.tsx
+++ app/shop/category/[slug]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
+import { generateStaticParams } from './generateStaticParams'
 
+export { generateStaticParams }
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
