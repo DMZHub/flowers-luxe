b4c96951ec1fd3669127c5fb549107f1c5a3cc3e
DMZHub <mostafaalfadil63@gmail.com>
2025-03-26 07:03:39 +0200

    Create generateStaticParams.ts

---
+++ app/shop/category/[slug]/generateStaticParams.ts
@@ -0,0 +1,11 @@
+export function generateStaticParams() {
+  return [
+    { slug: 'throw-pillows' },
+    { slug: 'stickers' },
+    { slug: 'mugs' },
+    { slug: 'art' },
+    { slug: 'tote-bags' },
+    { slug: 'tapestries' },
+    { slug: 'pins' }
+  ]
+}
