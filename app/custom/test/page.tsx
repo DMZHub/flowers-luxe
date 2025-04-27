c763a800343051c765480a5d0f1646d96f49f3be
DMZHub <mostafaalfadil63@gmail.com>
2025-04-27 10:28:15 +0300

    Create page.tsx
    TEST

---
+++ app/custom/test/page.tsx
@@ -0,0 +1,13 @@
+// app/custom/test/page.tsx
+"use client"
+
+import React from 'react'
+
+export default function TestPage() {
+  return (
+    <div className="container-custom py-16">
+      <h1 className="text-2xl font-bold mb-4">Test Custom Page</h1>
+      <p>If you can see this, the custom routing is working correctly.</p>
+    </div>
+  )
+}
