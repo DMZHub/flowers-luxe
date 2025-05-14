85e492aa95e403aeb018cda0dcf09442a694f817
DMZHub <mostafaalfadil63@gmail.com>
2025-05-14 15:20:19 +0300

    Update route.ts remov wwww

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -8,31 +8,31 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://www.flowersluxe.com/</loc>
+    <loc>https://flowersluxe.com/</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://www.flowersluxe.com/about</loc>
+    <loc>https://flowersluxe.com/about</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://www.flowersluxe.com/shop</loc>
+    <loc>https://flowersluxe.com/shop</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://www.flowersluxe.com/blog</loc>
+    <loc>https://flowersluxe.com/blog</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://www.flowersluxe.com/contact</loc>
+    <loc>https://flowersluxe.com/contact</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
