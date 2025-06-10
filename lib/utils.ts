f6108baf36bf40917827d1e3fd4b91817458bdcd
DMZHub <mostafaalfadil63@gmail.com>
2025-06-10 18:19:44 +0300

    Delete lib directory

--- lib/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Combines multiple class names into a single string
- * This is a simplified version of the clsx/classnames utility
- */
-export function cn(...classes: (string | boolean | undefined | null)[]) {
-  return classes.filter(Boolean).join(' ');
-}
