1f639cd4b826d81a0dd4ce4e12a1b1546745577c
DMZHub <mostafaalfadil63@gmail.com>
2025-06-10 18:19:15 +0300

    Delete  all components directory

--- components/NewsletterSignup.tsx
+++
@@ -1,9 +0,0 @@
-// components/NewsletterSignup.tsx
-"use client"
-import React, { useState, FormEvent, ChangeEvent } from 'react'
-import * as emailjs from 'emailjs-com'
-import { Send, CheckCircle, AlertCircle } from 'lucide-react'
-
-export default function NewsletterSignup() {
-  // ... (same implementation as in previous discussions)
-}
