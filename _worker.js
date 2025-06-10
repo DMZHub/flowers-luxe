7a0a8c82021f4eebaff669524f5448d5d35cdd0a
DMZHub <mostafaalfadil63@gmail.com>
2025-06-10 18:21:54 +0300

    Delete _worker.js

--- _worker.js
+++
@@ -1,8 +0,0 @@
-// _worker.js - Force Cloudflare Pages to use Node.js 18.18.0
-export default {
-    async fetch(request, env) {
-      return new Response("Hello from Cloudflare Workers!", {
-        headers: { "content-type": "text/plain" },
-      });
-    },
-  };
