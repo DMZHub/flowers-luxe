f841f284c8499ec8df7afc328881076e318ab995
DMZHub <mostafaalfadil63@gmail.com>
2025-06-12 02:07:39 +0300

    Update ParamsWrapper.tsx

--- components/ParamsWrapper.tsx
+++ components/ParamsWrapper.tsx
@@ -1,9 +1,19 @@
 // components/ParamsWrapper.tsx
 import React from 'react';
 
-// Generic type for params - can be extended for different route types
+// Generic type for params
 export interface PageParams {
   [key: string]: string | string[] | undefined;
+}
+
+// Specific type interfaces for your routes
+export interface StylePageParams {
+  style: string;
+}
+
+export interface ProductPageParams {
+  style: string;
+  product: string;
 }
 
 // Props for the wrapper component
@@ -33,38 +43,4 @@ export default async function ParamsWrapper<T extends PageParams>({
       searchParams={resolvedSearchParams}
     />
   );
-}
-
-// Specific type interfaces for your routes
-export interface StylePageParams {
-  style: string;
-}
-
-export interface ProductPageParams {
-  style: string;
-  product: string;
-}
-
-// Helper function to create wrapped page components
-export function createWrappedPage<T extends PageParams>(
-  OriginalComponent: React.ComponentType<{
-    params: T;
-    searchParams?: { [key: string]: string | string[] | undefined };
-  }>
-) {
-  return function WrappedPage({
-    params,
-    searchParams
-  }: {
-    params: Promise<T>;
-    searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
-  }) {
-    return (
-      <ParamsWrapper
-        params={params}
-        searchParams={searchParams}
-        Component={OriginalComponent}
-      />
-    );
-  };
 }
