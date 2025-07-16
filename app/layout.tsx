06f266d630276fb4e2f67b2527ea43c15e6a1820
DMZHub <mostafaalfadil63@gmail.com>
2025-07-17 01:53:02 +0300

    Update layout.tsx  Google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,17 @@ export default function RootLayout({
       <head>
         {/* Preconnect to external domains for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /> 
+
+        <!-- Google tag (gtag.js) -->
+       <script async src="https://www.googletagmanager.com/gtag/js?id=G-FGVRG6MC02"></script>
+       <script>
+       window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
+
+       gtag('config', 'G-FGVRG6MC02');
+       </script>
         
         {/* Additional SEO meta tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
